57597517066584aea06580edf705e1f7715ddc29
Ann-Marie Thompson <amt@Ann-Maries-MacBook-Pro.local>
2018-10-21 21:56:23 -0400

    functions

--- FlashcardDriver.py
+++ FlashcardDriver.py
@@ -1,10 +1,27 @@
+import webbrowser as web
 from Flashcards import *
-from functions import * 
+from functions import *
 
+# Initalize index counter
+i = 0
+
+# Print list of dict study options
 print("Welche Kartenstapel möchtest du studieren?")
-print('Halloween\nBirthdays\nStudies\nVerbs\nIrregular Verbs\nAdjectives\n')
-study_answer = input()
+printList()
+    
+# Take user input on which dict to study
+study_answer = input().lower()
 
+# Ask to review or to study
+print("Would you like to study or review?")
+response = input().lower()
+
+# Check for dict and then select if there
 if study_answer in string_list_of_dictionaries:
     index = string_list_of_dictionaries.index(study_answer)
+
+# Call the study function to begin listing dict elements
+if response == 'review' or 'Review':
+    review(list_of_dicts[index])
+else:
     study(list_of_dicts[index])
--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
-# Main function for stuying a dictionary
+import webbrowser as web
+from Flashcards import *
 
-def study(dictionary): # Takes input of a dictionary
+# Main function for studying a dict
+
+def study(dictionary): # Takes input of a dict
     i = 0
     l = len(dictionary)
     while i < l:
@@ -12,6 +15,42 @@ def study(dictionary): # Takes input of a dictionary
                 if answer == dictionary[key]:
                     print('Korrekt!')
                     break
+                elif answer == "hilfe":
+                    getHelp(key)
                 else:
                     print('Inkorrekt!')
                     correct = False
+
+# Main function for reviewing a dict   
+def review(dictionary): # Takes input of a dict
+    i = 0
+    l = len(dictionary)
+    while i < l:
+        for key in dictionary:
+                print("Deutsch: " + key + "\nEnglisch: " + dictionary[key])
+                answer = input()
+                if answer == 'done':
+                    break
+
+# Function to print list of available dicts for study                
+def printList(): # No input necessary
+    i = 0
+    for name in string_list_of_dictionaries:
+        print(string_list_of_dictionaries[i].title())
+        i += 1
+
+
+# Function to open webpage to German dictionary for help
+def getHelp(word):
+    word = word.replace(' ', '%20')
+    url = 'https://dict.leo.org/german-english/' + word ### Curent issue with ascii and utf-8
+    web.open(url)
+
+
+
+
+
+    
+
+                
+    
